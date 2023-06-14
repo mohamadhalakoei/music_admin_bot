214bdbc465ef63f94382bd5d22de91fdc79aabf9
Mohamad Halakoei <exploit.0.ir@gmail.com>
2023-06-14 22:48:59 +0330

    creating a music download section

--- admin_bot.py
+++ admin_bot.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 load_dotenv()
 
@@ -41,6 +41,21 @@ def check_password(user_id, args):
         else:
             return False
 
+def download_audio(update, context):
+    user_id = update.effective_user.id
+    if check_password(user_id, context.args):
+        audio_file = update.message.audio
+        file_id = audio_file.file_id
+        file_name = audio_file.file_name
+        artist = file_name.split('-')[0]
+        title = file_name.split('-')[1][0:-4]
+        new_file = context.bot.get_file(file_id)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"downloading...")
+        new_file.download(MUSIC_PATH + f'{title.strip()}.mp3')
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"downloaded...")
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="You don't have access.")
+
 def main():
     # create an Updater object and attach it to your bot's token
     updater = Updater(token=TOKEN, use_context=True)
@@ -50,6 +65,7 @@ def main():
 
     # register a handler for the all command
     dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(Filters.audio, download_audio))
 
     # start the bot
     updater.start_polling()
