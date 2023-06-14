c0a7dc7d6e562369e227449eac6d455272bfcd71
Mohamad Halakoei <exploit.0.ir@gmail.com>
2023-06-14 17:25:50 +0330

    Create a bot

--- admin_bot.py
+++ admin_bot.py
@@ -1,6 +1,28 @@
 import os
 from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
+
+# Command Functions
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, welcome to the admin bot of the channel!")
+
+def main():
+    # create an Updater object and attach it to your bot's token
+    updater = Updater(token=TOKEN, use_context=True)
+
+    # create a dispatcher object to register handlers
+    dispatcher = updater.dispatcher
+
+    # register a handler for the all command
+    dispatcher.add_handler(CommandHandler('start', start))
+
+    # start the bot
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
